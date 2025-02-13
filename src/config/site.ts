55d5bf04fc7714bf2d06a1dfd80c7c6bd056f380
FL232323 <Fosterleboeuf23@gmail.com>
2025-02-13 07:44:10 -0500

    Update site configuration with betting focus

---
+++ src/config/site.ts
@@ -0,0 +1,14 @@
+export const siteConfig = {
+  name: "Bet Analytics Pro",
+  description:
+    "One-click analysis of your Hard Rock Bet history. Get instant insights into your betting performance.",
+  url: "https://betanalytics.pro",
+  ogImage: "https://betanalytics.pro/og.jpg",
+  links: {
+    twitter: "https://twitter.com/betanalyticspro",
+    github: "https://github.com/FL232323/betting-analytics-new",
+  },
+  mailSupport: "support@betanalytics.pro"
+}
+
+export type SiteConfig = typeof siteConfig
